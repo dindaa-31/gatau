dd78165b20317016aff943eec12592bd73480ebc
dindaa-31 <dmaharani3108@gmail.com>
2024-08-14 23:11:47 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'halooo'.split('')
+const text = 'mba heheheheh'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
