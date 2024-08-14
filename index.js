343a40ccab769f6ad9d6aa9f2c1b300f2f6f0f81
dindaa-31 <dmaharani3108@gmail.com>
2024-08-14 23:36:58 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'mba heheheheh'.split('')
+const text = 'berikan adikmu tambahan tuk main di Kidzania ya😊 klo udh kuliah kan jarang bisa main lg🥺'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
